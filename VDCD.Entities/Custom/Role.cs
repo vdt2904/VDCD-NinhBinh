61b48cb5c65e1a7ef541de0f39a4437af85864b7
vdt2904 <vudoanhthai12b2002hla@gmail.com>
2026-02-26 14:51:39 +0700

    Feat add

---
+++ VDCD.Entities/Custom/Role.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VDCD.Entities.Custom
+{
+	public class Role
+	{
+		public int RoleID { get; set; }
+		public string? RoleKey { get; set; }
+		public string? RoleName { get; set; }
+		public string? Desctiption { get; set; }
+		public bool IsActive { get; set; } = true;
+	}
+}
---
+++ VDCD.Entities/Custom/UserRole.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VDCD.Entities.Custom
+{
+	public class UserRole
+	{
+		public int Id { get; set; }	
+		public int RoleId { get; set; }
+		public int UserId { get; set; }
+	}
+}

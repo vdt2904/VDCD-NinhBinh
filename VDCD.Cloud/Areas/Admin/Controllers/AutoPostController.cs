98db3c6cd7eefa151e4aee8b5ba43483f91623e7
minhkhiet2003 <khiet2003@gmail.com>
2026-02-27 10:53:33 +0700

    add user permissions

--- VDCD.Cloud/Areas/Admin/Controllers/AutoPostController.cs
+++ VDCD.Cloud/Areas/Admin/Controllers/AutoPostController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using VDCD.Business.Service;
 using VDCD.Entities.Security;
 
 namespace VDCD.Areas.Admin.Controllers
@@ -8,8 +9,15 @@ namespace VDCD.Areas.Admin.Controllers
     [Authorize(AuthenticationSchemes = "AdminAuth", Roles = AdminRoles.ContentAccess)]
     public class AutoPostController : Controller
     {
+        private UserService _userService;
+        public AutoPostController(UserService userService)
+        {
+            _userService = userService;
+        }
+
         public IActionResult Index()
         {
+            ViewBag.ListUser = _userService.GetUsers();
             return View();
         }
     }

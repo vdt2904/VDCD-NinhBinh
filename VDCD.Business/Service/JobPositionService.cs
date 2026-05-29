45410f32605c593c6a216914a60b3ab9bea6301c
Minh Khiet <khiet2003@gmail.com>
2026-05-29 08:44:36 +0700

    fate: add filter JobPosition

--- VDCD.Business/Service/JobPositionService.cs
+++ VDCD.Business/Service/JobPositionService.cs
@@ -77,7 +77,8 @@ namespace VDCD.Business.Service
                 entity.ExpiredDate = model.ExpiredDate;
                 entity.Slug = model.Slug;
                 entity.Salary = model.Salary;
-
+                entity.Industry = model.Industry;
+                entity.Level = model.Level;
                 _jobPositionRepo.Update(entity);
             }
 
